4b48bcbf6d19b475ac62d717133cd58b9857480d
Philo van Kemenade <pvkemenade@beeldengeluid.nl>
2022-10-05 14:50:33 +0200

    replace ghost for elf emoji in chat message

--- dane_workflows/status_monitor.py
+++ dane_workflows/status_monitor.py
@@ -342,7 +342,7 @@ class SlackStatusMonitor(StatusMonitor):
         slack_client.chat_postMessage(
             channel=self.config["CHANNEL"],
             blocks=formatted_status,
-            icon_emoji=":ghost:",
+            icon_emoji=":female_elf:",
         )
 
         if formatted_error_report:  # only upload error file if has content
